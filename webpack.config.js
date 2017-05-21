578ef9c7a078aeef94e8603c28f6f587ca3aa015
Alex Villacres <alex@Alexs-MacBook-Pro.local>
2017-05-20 18:25:28 -0700

    First commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
-
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: ['./client/client.js'],
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders:
+    [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
